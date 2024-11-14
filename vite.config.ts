f252e2e1cf6c4d3efc4ce9dc584accb268a7c419
Reuben Sebastian Joseph <135094076+reuben809@users.noreply.github.com>
2024-11-14 22:07:29 +0530

    Updated vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // Create a separate chunk for dependencies in node_modules to reduce main bundle size
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    // Adjust chunk size warning limit (optional)
+    chunkSizeWarningLimit: 1000, // Set to 1000 kB to suppress warning for large chunks
+  },
 });
